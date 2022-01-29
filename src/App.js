85aa3692078fa01bfa0839694b8ecd9b0cd7b07e
Haoran Zhou <kens0serious@gmail.com>
2022-01-29 18:09:41 -0500

    finished slider

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import { FaQuoteRight } from 'react-icons/fa'
 import data from './data'
 function App() {
   const [people, setPeople] = useState(data)
-  const [index, setindex] = useState()
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    if (index > people.length - 1) {
+      setIndex(0)
+    } else if (index < 0) {
+      setIndex(people.length - 1)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 3000)
+
+    return () => {
+      clearInterval(slider)
+    }
+  }, [index])
+
   return (
     <section className="section">
       <div className="title">
@@ -14,10 +33,20 @@ function App() {
       </div>
 
       <div className="section-center">
-        {people.map((ppl) => {
+        {people.map((ppl, pplIndex) => {
           const { id, name, image, title, quote } = ppl
+          let position = 'nextSlide'
+          if (
+            pplIndex === index - 1 ||
+            (index === 0 && pplIndex === people.length - 1)
+          ) {
+            position = 'lastSlide'
+          }
+          if (index === pplIndex) {
+            position = 'activeSlide'
+          }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -26,10 +55,10 @@ function App() {
             </article>
           )
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft></FiChevronLeft>
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight></FiChevronRight>
         </button>
       </div>
